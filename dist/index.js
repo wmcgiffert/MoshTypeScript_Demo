369775b77d47d1580e5b5479455ae2996dabcae8
wmcgiffert <wmcgiffert@crimson.ua.edu>
2023-04-20 16:25:17 -0500

    inital push

---
+++ dist/index.js
@@ -0,0 +1,18 @@
+"use strict";
+;
+let mySize = 2;
+console.log(mySize);
+let user = [1, 'Mosh'];
+user.push(1);
+let numbers = [1, 2, 3];
+let sales = 281330;
+let course = 'TypeScript';
+let is_published = true;
+let level;
+level = 1;
+level = 'a';
+console.log(sales, course, is_published, level);
+function render(document) {
+    console.log(document);
+}
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,28 @@
+// ========== 7 Enums ==========
+const enum Size {Small = 1, Medium, Large};
+let mySize: Size = Size.Medium;
+console.log(mySize);
+
+// ========== 6 Tuples ==========
+let user: [number, string] = [1,'Mosh'];
+user.push(1);
+
+// ========== 5 Arrays ==========
+let numbers: number[] = [1,2,3];
+
+
+// ========== 5 Any Type ==========
+let sales: number = 281_330;
+let course: string = 'TypeScript';
+let is_published: boolean = true;
+let level;
+level = 1;
+level = 'a';
+
+
+
+console.log(sales,course,is_published,level);
+
+function render(document:any){
+    console.log(document);
+}
