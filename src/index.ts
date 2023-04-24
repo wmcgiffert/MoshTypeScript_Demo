853de38610776612c053a21fb1ac2203bfc38363
wmcgiffert <wmcgiffert@crimson.ua.edu>
2023-04-24 09:59:41 -0500

    section 8 Functions

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+// ========== 8 Functions ==========
+function calculateTax(income: number, taxYear: number): number{
+    if(taxYear < 2023)
+        return income * 1.2;
+    return income * 1.3;
+}
+
 // ========== 7 Enums ==========
 const enum Size {Small = 1, Medium, Large};
 let mySize: Size = Size.Medium;
