96b7c8ae5b57aff6da92822f12db8b53d0a50795
wmcgiffert <wmcgiffert@crimson.ua.edu>
2023-04-24 14:28:44 -0500

    Advanced Types 2 Type Aliases

---
+++ dist/AdvancedTypes.js
@@ -0,0 +1,9 @@
+"use strict";
+let employee2 = {
+    id: 1,
+    name: '',
+    retire: (date) => {
+        console.log(date);
+    }
+};
+//# sourceMappingURL=AdvancedTypes.js.map
--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,17 @@
 "use strict";
+let employee = {
+    id: 1,
+    name: '',
+    retire: (date) => {
+        console.log(date);
+    }
+};
+function calculateTax(income, taxYear = 2022) {
+    if (taxYear < 2023)
+        return income * 1.2;
+    return income * 1.3;
+}
+calculateTax(50000);
 ;
 let mySize = 2;
 console.log(mySize);
