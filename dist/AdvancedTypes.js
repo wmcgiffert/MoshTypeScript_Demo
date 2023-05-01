6469ce4546e2b1d7ba7171ea15023038b333e4e2
wmcgiffert <wmcgiffert@crimson.ua.edu>
2023-05-01 11:25:10 -0500

    CIFOOP 1 Intro to Classes Interfeaces and Object Oriented Programming

--- dist/AdvancedTypes.js
+++ dist/AdvancedTypes.js
@@ -1,4 +1,5 @@
 "use strict";
+var _a;
 let employee2 = {
     id: 1,
     name: '',
@@ -17,4 +18,39 @@ function kgToLbs(weight) {
 const weight1 = kgToLbs(10);
 const weight2 = kgToLbs('10kg');
 console.log(weight1, weight2);
+let textBox = {
+    drag: () => { },
+    resize: () => { }
+};
+let quantity = 50;
+function greet(name) {
+    if (name) {
+        console.log(name.toUpperCase());
+    }
+    else {
+        console.log('Hola');
+    }
+}
+greet(null);
+function getCustBirthDate(id) {
+    return id === 0 ? null : { birthDate: new Date() };
+}
+let customer = getCustBirthDate(0);
+console.log((_a = customer === null || customer === void 0 ? void 0 : customer.birthDate) === null || _a === void 0 ? void 0 : _a.toDateString());
+let log = null;
+log === null || log === void 0 ? void 0 : log('a');
+let speed = null;
+let ride = {
+    speed: speed !== null && speed !== void 0 ? speed : 30
+};
+let phone = document.getElementById('phone');
+phone.value = '123-456-7890';
+function reject(message) {
+    throw new Error(message);
+}
+function processEvents() {
+    while (true) {
+    }
+}
+reject('Error');
 //# sourceMappingURL=AdvancedTypes.js.map
