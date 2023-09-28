29745fa23f85ce416441c193fdf9af1a5933e5bd
wmcgiffert <wmcgiffert@crimson.ua.edu>
2023-09-28 13:38:37 -0500

    End of Section 3 Generic Classes

--- dist/Generics.js
+++ dist/Generics.js
@@ -9,10 +9,9 @@ class KeyValuePair {
 let pair1 = new KeyValuePair(1, 'a');
 let pair2 = new KeyValuePair(2, 'b');
 let pair3 = new KeyValuePair(3, 'c');
+let pair4 = new KeyValuePair(4, 'd');
 let pairs = [pair1, pair2];
 pairs.push(pair3);
-pairs.forEach(pair => {
-    console.log(pair);
-});
+pairs.push(pair4);
 console.table(pairs);
 //# sourceMappingURL=Generics.js.map
--- src/Generics.ts
+++ src/Generics.ts
@@ -6,13 +6,15 @@ class KeyValuePair<K, V> {
 let pair1 = new KeyValuePair<number, string>(1, 'a');
 let pair2 = new KeyValuePair<number, string>(2, 'b');
 let pair3 = new KeyValuePair<number, string>(3, 'c');
+let pair4 = new KeyValuePair(4, 'd');
 
 let pairs = [pair1,  pair2]; 
 pairs.push(pair3); 
-
-pairs.forEach(pair => {
-    console.log(pair);
-});
+pairs.push(pair4); 
+// Interesting hack throws an ts error but runs in js
+// pairs.forEach(pair => {
+//     console.log(pair);
+// });
 
 console.table(pairs);
 
