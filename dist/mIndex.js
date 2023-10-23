d9d73d140e85a8c623e8bee5612f503f1d491baa
wmcgiffert <wmcgiffert@crimson.ua.edu>
2023-10-23 10:37:04 -0500

    End of Section 2 Modules

--- dist/mIndex.js
+++ dist/mIndex.js
@@ -1,8 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const TsModules_1 = require("./TsModules");
-let circle = new TsModules_1.MCircle(1);
+let circle = new TsModules_1.MCircle(2);
 console.log(circle);
-let square = new TsModules_1.Square(1);
-console.log(square);
 //# sourceMappingURL=mIndex.js.map
